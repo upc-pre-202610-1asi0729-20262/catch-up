c57b341a75759c97119d5805b782d7ca2aa10dba
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-20 09:41:14 -0500

    feat(top-headlines): add article assembler class to convert article resources to entities.

---
+++ src/app/news/infrastructure/article-assembler.ts
@@ -0,0 +1,31 @@
+import { LogoDevApi } from '../../shared/infrastructure/logo-dev-api';
+import { ArticleResource, TopHeadlinesResponse } from './top-headlines-response';
+import { Article } from '../domain/model/article.entity';
+
+export class ArticleAssembler {
+  static logoApi: LogoDevApi;
+
+  static withLogoApi(logoApi: LogoDevApi) {
+    this.logoApi = logoApi;
+  }
+
+  static toEntityFromResource(resource: ArticleResource): Article {
+    return {
+      source: {
+        id: resource.source.id || '',
+        name: resource.source.name,
+        url: '',
+        urlToLogo: ''
+      },
+      title: resource.title,
+      description: resource.description || '',
+      url: resource.url,
+      urlToImage: resource.urlToImage || '',
+      publishedAt: resource.publishedAt
+    }
+  }
+
+  static toEntitiesFromResponse(response: TopHeadlinesResponse): Article[] {
+    return response.articles.map(article => this.toEntityFromResource(article));
+  }
+}
