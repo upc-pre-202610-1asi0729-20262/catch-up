7abc88bac579d082e3e9dc7bc02ce1ae0a9cd086
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-20 08:45:27 -0500

    feat(layout): integrate layout component into app structure and update dependencies.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Layout } from './shared/presentation/components/layout/layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Layout],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('catch-up');
