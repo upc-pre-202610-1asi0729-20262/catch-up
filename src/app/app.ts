6627de16b7ecf86294fbfb5192302de965df69be
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-20 08:58:23 -0500

    feat(app): remove router outlet import from app component.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Layout } from './shared/presentation/components/layout/layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Layout],
+  imports: [Layout],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
