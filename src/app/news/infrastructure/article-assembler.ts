98e6e937e158169f7a29cba784b3ea5181ff7c6d
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-22 07:35:37 -0500

    feat(articles): add return statement to withLogoApi method.

--- src/app/news/infrastructure/article-assembler.ts
+++ src/app/news/infrastructure/article-assembler.ts
@@ -7,6 +7,7 @@ export class ArticleAssembler {
 
   static withLogoApi(logoApi: LogoDevApi) {
     this.logoApi = logoApi;
+    return this;
   }
 
   static toEntityFromResource(resource: ArticleResource): Article {
